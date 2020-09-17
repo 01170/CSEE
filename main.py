80ef14bd378a2368bbeb0d8a4258bdb988c6f75c
01170 <67077346+01170@users.noreply.github.com>
2020-09-16 23:56:00 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 #This is my CSEE project
 
 import pandas as pd
-import pip
-import sklearn as sklearn
 import numpy as np
-import tweepy
 
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-#from Tweet import Tweet
+
+num_of_days = 7
 
 #Store data in variable
 df = pd.read_csv('TSLA.csv', usecols=['Date', 'Open', 'Close'])
@@ -24,26 +20,38 @@ df = df.set_index(pd.DatetimeIndex(df['Date'].values))
 #Name index
 df.index.name = 'Date'
 
-#for loop
-#for x in df:
-    #print(df['Close'])
-    #print(df['Open'])
+# Initialize the regressors (x) and the predictors (y).
+x = np.array([])
+y = np.array([])
+
+# Iterate over the data setting the regressors as the open values
+# and the predictors as the closing values.
+for i, j in df.iterrows():
+  x = np.append(x, j['Open'])
+  y = np.append(y, j['Close'])
+
+# Setup the model
+x = x.reshape((-1, 1))
+model = LinearRegression()
+model.fit(x, y)
+
+# Print some useful info
+print('coefficient of determination:', model.score(x, y))
+print('predicted response for 200:', model.predict([[200]]))
+print('slope:', model.coef_)
+
+# Build out some number of days using previous close values as new open values
+new_open = model.predict([[df.iloc[len(df) - 1]['Close']]])
+z = np.array([])
+for x in range(num_of_days):
+  p = model.predict([new_open])
+  new_open = p
+  z = np.append(z, p)
 
 #Create target column
-df['Price_Up'] = np.where(df['Close'].shift(-1) > df["Close"], 1, 0)
+df['PriceUp'] = np.where(df['Close'].shift(-1) > df["Close"], 1, 0)
 
 #Remove additional Date column
 df = df.drop(columns=['Date'])
 print(df)
-
-x = np.array([5, 15, 25, 35, 45, 55]).reshape((-1, 1))
-y = np.array([5, 20, 14, 32, 22, 38])
-
-model = LinearRegression()
-#model.fit(x, y)
-#r_sq = model.score(x, y)
-
-#print('coefficient of determination:', r_sq)
-
-#y_pred = model.predict(x)
-#print('predicted response:', y_pred, sep='\n')
+print(z)
